3ada178d9d392bfad86a10d797b89ca2365cec9a
Allgm <98173877+Allgm@users.noreply.github.com>
2024-07-10 17:48:29 -0600

    Seleccion de meses ultimo reporte

--- src/views/base/Collapses.vue
+++ src/views/base/Collapses.vue
@@ -1,142 +1,214 @@
 <template>
-  <CRow>
-    <CCol :xs="12">
-      <CCard class="mb-4">
-        <CCardHeader>
-          <strong>Vue Collapse</strong>
-        </CCardHeader>
-        <CCardBody>
-          <p class="text-body-secondary small">
-            You can use a link or a button component.
-          </p>
-          <DocsExample href="components/collapse.html#example">
-            <CButton color="primary" href="#" @click="visible = !visible"
-              >Link</CButton
-            >
-            <CButton color="primary" @click="visible = !visible"
-              >Button</CButton
-            >
-            <CCollapse :visible="visible">
-              <CCard class="mt-3">
-                <CCardBody>
-                  Anim pariatur cliche reprehenderit, enim eiusmod high life
-                  accusamus terry richardson ad squid. Nihil anim keffiyeh
-                  helvetica, craft beer labore wes anderson cred nesciunt
-                  sapiente ea proident.
-                </CCardBody>
-              </CCard>
-            </CCollapse>
-          </DocsExample>
-        </CCardBody>
-      </CCard>
-      <CCard class="mb-4">
-        <CCardHeader>
-          <strong>Vue Collapse</strong> <small> Horizontal</small>
-        </CCardHeader>
-        <CCardBody>
-          <p class="text-body-secondary small">
-            The collapse plugin also supports horizontal collapsing. Add the
-            <code>horizontal</code> property to transition the
-            <code>width</code> instead of <code>height</code> and set a
-            <code>width</code> on the immediate child element.
-          </p>
-          <DocsExample href="components/collapse.html#horizontal">
-            <CButton
-              class="mb-3"
-              color="primary"
-              aria-expanded="{visible}"
-              aria-controls="collapseWidthExample"
-              @click="visibleHorizontal = !visibleHorizontal"
-              >Button</CButton
-            >
-            <div style="min-height: 120px">
-              <CCollapse horizontal :visible="visibleHorizontal">
-                <CCard style="width: 300px">
-                  <CCardBody>
-                    This is some placeholder content for a horizontal collapse.
-                    It's hidden by default and shown when triggered.
-                  </CCardBody>
-                </CCard>
-              </CCollapse>
-            </div>
-          </DocsExample>
-        </CCardBody>
-      </CCard>
-      <CCard class="mb-4">
-        <CCardHeader>
-          <strong>Vue Collapse</strong> <small> multi target</small>
-        </CCardHeader>
-        <CCardBody>
-          <p class="text-body-secondary small">
-            A <code>&lt;CButton&gt;</code> can show and hide multiple elements.
-          </p>
-          <h4 class="mt-4">Toggle multiple targets</h4>
-          <DocsExample href="components/collapse.html#multiple-targets">
-            <CButton color="primary" @click="visibleA = !visibleA"
-              >Toggle first element</CButton
-            >
-            <CButton color="primary" @click="visibleB = !visibleB"
-              >Toggle second element</CButton
-            >
-            <CButton
-              color="primary"
-              @click="
-                () => {
-                  visibleA = !visibleA
-                  visibleB = !visibleB
-                }
-              "
-            >
-              Toggle both elements
-            </CButton>
-            <CRow>
-              <CCol :xs="6">
-                <CCollapse :visible="visibleA">
-                  <CCard class="mt-3">
-                    <CCardBody>
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life
-                      accusamus terry richardson ad squid. Nihil anim keffiyeh
-                      helvetica, craft beer labore wes anderson cred nesciunt
-                      sapiente ea proident.
-                    </CCardBody>
-                  </CCard>
-                </CCollapse>
-              </CCol>
-              <CCol :xs="6">
-                <CCollapse :visible="visibleB">
-                  <CCard class="mt-3">
-                    <CCardBody>
-                      Anim pariatur cliche reprehenderit, enim eiusmod high life
-                      accusamus terry richardson ad squid. Nihil anim keffiyeh
-                      helvetica, craft beer labore wes anderson cred nesciunt
-                      sapiente ea proident.
-                    </CCardBody>
-                  </CCard>
-                </CCollapse>
-              </CCol>
-            </CRow>
-          </DocsExample>
-        </CCardBody>
-      </CCard>
-    </CCol>
-  </CRow>
+  <div>
+    <!-- Primera división -->
+    <div class="division-container">
+      <div class="nombre-fecha-container">
+        <div class="id-inputs">
+          <label>Período de informe</label>
+          <select v-model="selectedPeriodo" @change="actualizarMeses">
+            <option v-for="periodo in periodos" :key="periodo" :value="periodo">{{ periodo }}</option>
+          </select>
+          <label>Mes</label>
+          <select v-model="selectedMes">
+            <option v-for="mes in meses" :key="mes" :value="mes">{{ mes }}</option>
+          </select>
+        </div>
+        <div class="nombre-inputs">
+          <label>Contador</label>
+          <div class="numero-input">
+            <input type="text" v-model="contador">
+          </div>
+        </div>
+        <div class="nombre-inputs">
+          <label>Responsable de proyecto Agrícola</label>
+          <div class="numero-input">
+            <input type="text" v-model="responsableAgricola">
+          </div>
+        </div>
+        <div class="nombre-inputs">
+          <label>Economa provincial</label>
+          <div class="numero-input">
+            <input type="text" v-model="economaProvincial">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Espacio entre la división 3 y el botón -->
+    <div style="margin-top: 20px;"></div>
+
+    <!-- Botón Agregar -->
+    <button @click="insertar">Guardar</button>
+    <button @click="actualizar" style="margin-left: 10px;">Actualizar</button>
+    <button @click="limpiar" style="margin-left: 10px;">Limpiar</button>
+  </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import axios from 'axios';
+import { ref, reactive } from 'vue';
+
 export default {
-  name: 'Collapse',
+  name: 'Modals',
   setup() {
-    const visible = ref(false)
-    const visibleA = ref(false)
-    const visibleB = ref(false)
-    const visibleHorizontal = ref(false)
+    const contador = ref('');
+    const responsableAgricola = ref('');
+    const economaProvincial = ref('');
+    const selectedPeriodo = ref('');
+    const selectedMes = ref('');
+    const periodos = ['Mensual', 'Trimestral', 'Semestral', 'Anual'];
+    const meses = ref([]);
+
+    const actualizarMeses = () => {
+      switch (selectedPeriodo.value) {
+        case 'Mensual':
+          meses.value = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
+          break;
+        case 'Trimestral':
+          meses.value = ['Enero', 'Abril', 'Julio', 'Octubre'];
+          break;
+        case 'Semestral':
+          meses.value = ['Enero', 'Julio'];
+          break;
+        case 'Anual':
+          meses.value = ['Enero'];
+          selectedMes.value = 'Enero';  // Seleccionar automáticamente "Enero"
+          break;
+      }
+    };
+
+
+    const limpiar = () => {
+      banco.value = '';
+      selectedPeriodo.value = '';
+      selectedMes.value = '';
+      contador.value = '';
+      responsableAgricola.value = '';
+      economaProvincial.value = '';
+    };
+
+
     return {
-      visible,
-      visibleA,
-      visibleB,
-      visibleHorizontal,
-    }
+      contador,
+      responsableAgricola,
+      economaProvincial,
+      selectedPeriodo,
+      selectedMes,
+      periodos,
+      meses,
+      limpiar,
+      actualizarMeses
+    };
   },
-}
+};
 </script>
+
+<style scoped>
+/* Estilos para el contenedor principal */
+.container {
+  max-width: 600px;
+  margin: 0 auto;
+  padding: 20px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+}
+
+/* Estilos para las divisiones */
+.division-container {
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 10px;
+  margin-top: 10px;
+}
+
+/* Estilos para las etiquetas */
+label {
+  display: block;
+  margin-bottom: 5px;
+}
+
+/* Estilos para los campos de entrada */
+input[type="text"],
+input[type="date"],
+select {
+  width: 100%;
+  padding: 8px;
+  margin-bottom: 10px;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  box-sizing: border-box;
+}
+
+/* Estilos para el botón */
+button {
+  padding: 10px 20px;
+  background-color: #14491b;
+  color: #fff;
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #475f27;
+}
+
+/* Estilos para los campos de entrada de número y select */
+.numero-fecha-container {
+  display: flex;
+}
+
+.numero-inputs,
+.fecha-inputs {
+  flex: 1;
+  margin-right: 10px;
+}
+
+.numero-inputs label,
+.fecha-inputs label {
+  display: block;
+}
+
+/* Estilos para los campos de entrada de número */
+.numero-input {
+  display: flex;
+}
+
+.numero-input input[type="text"] {
+  margin-right: 10px;
+}
+
+/* Estilos para los campos de entrada de fecha */
+.fecha-container {
+  flex: 1;
+  border: 1px solid #ccc;
+  border-radius: 5px;
+  padding: 5px;
+}
+
+.fecha-container label {
+  display: block;
+}
+
+.fecha-container select {
+  width: calc(100% - 10px);
+  border: none;
+  outline: none;
+}
+
+/* Estilos para los contenedores de entrada */
+.input-container {
+  display: flex;
+  align-items: center;
+}
+
+.input-container label {
+  width: 150px; /* Ancho fijo para las etiquetas */
+  margin-right: 10px;
+}
+
+.input-container select,
+.input-container input {
+  flex: 1;
+}
+</style>
