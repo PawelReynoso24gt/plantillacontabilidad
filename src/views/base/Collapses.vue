8ac33095b16bf07a50aa75a1fdc2badb65353a21
Allgm <98173877+Allgm@users.noreply.github.com>
2024-07-11 07:05:24 -0600

    Reporte final agricola

--- src/views/base/Collapses.vue
+++ src/views/base/Collapses.vue
@@ -1,6 +1,5 @@
 <template>
   <div>
-    <!-- Primera división -->
     <div class="division-container">
       <div class="nombre-fecha-container">
         <div class="id-inputs">
@@ -34,22 +33,21 @@
       </div>
     </div>
 
-    <!-- Espacio entre la división 3 y el botón -->
     <div style="margin-top: 20px;"></div>
 
-    <!-- Botón Agregar -->
-    <button @click="insertar">Guardar</button>
-    <button @click="actualizar" style="margin-left: 10px;">Actualizar</button>
+    <button @click="generarPDF">Generar PDF</button>
     <button @click="limpiar" style="margin-left: 10px;">Limpiar</button>
   </div>
 </template>
 
 <script>
 import axios from 'axios';
-import { ref, reactive } from 'vue';
+import { ref } from 'vue';
+import jsPDF from 'jspdf';
+import 'jspdf-autotable';
 
 export default {
-  name: 'Modals',
+  name: 'ReporteAG',
   setup() {
     const contador = ref('');
     const responsableAgricola = ref('');
@@ -72,14 +70,12 @@ export default {
           break;
         case 'Anual':
           meses.value = ['Enero'];
-          selectedMes.value = 'Enero';  // Seleccionar automáticamente "Enero"
+          selectedMes.value = 'Enero';
           break;
       }
     };
 
-
     const limpiar = () => {
-      banco.value = '';
       selectedPeriodo.value = '';
       selectedMes.value = '';
       contador.value = '';
@@ -87,6 +83,146 @@ export default {
       economaProvincial.value = '';
     };
 
+    const generarPDF = async () => {
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/in_eg/reporteFinalAG', {
+          tipo: selectedPeriodo.value.toLowerCase(),
+          mes: selectedMes.value.toLowerCase(),
+          contador: contador.value,
+          responsable: responsableAgricola.value,
+          economa: economaProvincial.value
+        });
+        const data = response.data;
+
+        const doc = new jsPDF();
+        let yOffset = 20; // Start position for text
+        const pageHeight = doc.internal.pageSize.height; // Height of a page
+        const pageMargin = 20; // Margin from the edge of the page
+
+        const addPageIfNeeded = () => {
+          if (yOffset > pageHeight - pageMargin) { // Check if there's enough space left
+            doc.addPage();
+            yOffset = 20; // Reset yOffset to top of the page
+          }
+        };
+
+        const addText = (text, x, y) => {
+          addPageIfNeeded();
+          doc.text(text, x, yOffset);
+          yOffset += 10; // Line height
+        };
+
+        const addTable = (head, body) => {
+          doc.autoTable({
+            head: [head],
+            body: body,
+            startY: yOffset,
+            theme: 'grid',
+            styles: {
+              cellPadding: 2.5,
+              fontSize: 8,
+              halign: 'center',
+              valign: 'middle',
+              overflow: 'linebreak'
+            },
+            headStyles: {
+              fillColor: [41, 128, 185],
+              textColor: [255, 255, 255]
+            }
+          });
+          yOffset = doc.autoTable.previous.finalY + 10; // Update yOffset after table
+        };
+
+        // Obtener año actual
+        const currentYear = new Date().getFullYear();
+
+        // Obtener mes actual
+        const currentMonth = new Date().toLocaleDateString('es-ES', { month: 'long' });
+
+        // Determinar título del informe
+        let periodoTexto;
+        if (selectedPeriodo.value === 'Mensual') {
+          periodoTexto = `RESUMEN DE ${selectedMes.value.toUpperCase()}`;
+        } else if (selectedPeriodo.value === 'Trimestral') {
+          const trimestre = {
+            'Enero': 'PRIMER TRIMESTRE',
+            'Abril': 'SEGUNDO TRIMESTRE',
+            'Julio': 'TERCER TRIMESTRE',
+            'Octubre': 'CUARTO TRIMESTRE'
+          };
+          periodoTexto = `RESUMEN ${trimestre[selectedMes.value] || ''}`;
+        } else if (selectedPeriodo.value === 'Semestral') {
+          periodoTexto = selectedMes.value === 'Enero' ? 'RESUMEN PRIMER SEMESTRE' : 'RESUMEN SEGUNDO SEMESTRE';
+        } else if (selectedPeriodo.value === 'Anual') {
+          periodoTexto = 'RESUMEN ANUAL';
+        }
+
+        // Agregar encabezado al PDF
+        doc.setFontSize(16);
+        doc.text(`REPORTE FINAL ${selectedPeriodo.value.toUpperCase()} ${currentYear}`, 105, 27, { align: 'center' });
+        doc.setLineWidth(0.5);
+        doc.line(60, 32, 150, 32); // Línea horizontal
+
+        doc.setFontSize(12);
+        yOffset = 40;
+        doc.text(`INFORME CORRESPONDIENTE AL`, 20, 40);
+        doc.text(periodoTexto, 91, 40);
+        doc.text(`DE`, 165, 40);
+        doc.text(`${currentYear}`, 175, 40);
+        doc.text(`PROYECTO AGRÍCOLA HOGAR SANTA LUISA`, 20, 50);
+        doc.text(`LUGAR:`, 130, 50);
+        doc.text(`QUETZALTENANGO`, 155, 50);
+        doc.text(`GUATEMALA`, 20, 60);
+        doc.text(`FECHA:`, 130, 60);
+        doc.text(new Date().toLocaleDateString('es-ES'), 160, 60);
+        
+        // Añadir espacio antes de la tabla
+        yOffset = 75; // Ajusta la posición Y después del encabezado
+
+        // Crear los datos para la tabla
+        const tableData = [
+          ['SALDO INICIAL', '', '', 'Q ' + data.saldo_inicial.toFixed(2)],
+          ['SALDO INICIAL EN CAJA GENERAL', '', 'Q ' + data.saldo_inicial_caja.toFixed(2), ''],
+          ['SALDO INICIAL EN BANCO', '', 'Q ' + data.saldo_inicial_bancos.toFixed(2), ''],
+          ['INGRESOS', '', '', 'Q ' + data.total_general_ingresos.toFixed(2)],
+          ['CAJA GENERAL', '', 'Q ' + data.total_ingresos_caja.toFixed(2), ''],
+          ...data.data_caja.filter(item => item.ingresos && parseFloat(item.ingresos) > 0).map(ingreso => [
+            ingreso.cuenta, 'Q ' + ingreso.ingresos, '', ''
+          ]),
+          ['BANCO', '', 'Q ' + data.total_ingresos_bancos.toFixed(2), ''],
+          ...data.data_bancos.filter(item => item.ingresos && parseFloat(item.ingresos) > 0).map(ingreso => [
+            ingreso.cuenta, 'Q ' + ingreso.ingresos, '', ''
+          ]),
+          ['EGRESOS', '', '', 'Q ' + data.total_general_egresos.toFixed(2)],
+          ['CAJA GENERAL', '', 'Q ' + data.total_egresos_caja.toFixed(2), ''],
+          ...data.data_caja.filter(item => item.egresos && parseFloat(item.egresos) > 0).map(egreso => [
+            egreso.cuenta, 'Q ' + egreso.egresos, '', ''
+          ]),
+          ['BANCO', '', 'Q ' + data.total_egresos_bancos.toFixed(2), ''],
+          ...data.data_bancos.filter(item => item.egresos && parseFloat(item.egresos) > 0).map(egreso => [
+            egreso.cuenta, 'Q ' + egreso.egresos, '', ''
+          ]),
+          ['SALDO FINAL', '', '', 'Q ' + data.total_saldo_final.toFixed(2)],
+          ['SALDO FINAL EN CAJA GENERAL', '', 'Q ' + data.total_saldo_final_caja.toFixed(2), ''],
+          ['SALDO FINAL EN BANCO', '', 'Q ' + data.total_saldo_final_bancos.toFixed(2), ''],
+          ['SUMAS IGUALES', '', 'Q ' + data.total_saldo_final.toFixed(2), 'Q ' + data.total_saldo_final.toFixed(2)]
+        ];
+
+        // Encabezado de la Tabla
+        const tableHeaders = ['Descripción', 'Detalle', 'Saldo suma', 'Suma'];
+
+        addTable(tableHeaders, tableData);
+        // Datos Finales
+        addText('Hecho por:', 20, yOffset);
+        addText('Contador: ' + data.contador, 20, yOffset);
+        addText('Responsable: ' + data.responsable, 20, yOffset);
+        addText('Economa: ' + data.economa, 20, yOffset);
+
+        doc.save('Reporte ingresos y egresos.pdf');
+      } catch (error) {
+        console.error(error);
+      }
+    };
 
     return {
       contador,
@@ -97,23 +233,19 @@ export default {
       periodos,
       meses,
       limpiar,
-      actualizarMeses
+      actualizarMeses,
+      generarPDF
     };
   },
 };
+
+
+
+
 </script>
 
-<style scoped>
-/* Estilos para el contenedor principal */
-.container {
-  max-width: 600px;
-  margin: 0 auto;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-}
 
-/* Estilos para las divisiones */
+<style scoped>
 .division-container {
   border: 1px solid #ccc;
   border-radius: 5px;
@@ -121,94 +253,45 @@ export default {
   margin-top: 10px;
 }
 
-/* Estilos para las etiquetas */
 label {
   display: block;
   margin-bottom: 5px;
 }
 
-/* Estilos para los campos de entrada */
 input[type="text"],
-input[type="date"],
 select {
   width: 100%;
   padding: 8px;
   margin-bottom: 10px;
   border: 1px solid #ccc;
   border-radius: 5px;
-  box-sizing: border-box;
 }
 
-/* Estilos para el botón */
 button {
+  background-color: #4CAF50;
+  color: white;
   padding: 10px 20px;
-  background-color: #14491b;
-  color: #fff;
   border: none;
   border-radius: 5px;
   cursor: pointer;
 }
 
 button:hover {
-  background-color: #475f27;
+  background-color: #45a049;
 }
 
-/* Estilos para los campos de entrada de número y select */
-.numero-fecha-container {
+.id-inputs {
   display: flex;
+  gap: 10px;
 }
 
-.numero-inputs,
-.fecha-inputs {
-  flex: 1;
-  margin-right: 10px;
+.nombre-inputs {
+  display: flex;
+  gap: 10px;
+  margin-top: 10px;
 }
 
-.numero-inputs label,
-.fecha-inputs label {
-  display: block;
-}
-
-/* Estilos para los campos de entrada de número */
 .numero-input {
-  display: flex;
-}
-
-.numero-input input[type="text"] {
-  margin-right: 10px;
-}
-
-/* Estilos para los campos de entrada de fecha */
-.fecha-container {
-  flex: 1;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  padding: 5px;
-}
-
-.fecha-container label {
-  display: block;
-}
-
-.fecha-container select {
-  width: calc(100% - 10px);
-  border: none;
-  outline: none;
-}
-
-/* Estilos para los contenedores de entrada */
-.input-container {
-  display: flex;
-  align-items: center;
-}
-
-.input-container label {
-  width: 150px; /* Ancho fijo para las etiquetas */
-  margin-right: 10px;
-}
-
-.input-container select,
-.input-container input {
   flex: 1;
 }
 </style>
